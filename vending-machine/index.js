e03e926df15bea56b4433ea3f7d8a6d0715f697f
leeyeun <dpdms0109@naver.com>
2022-11-19 23:21:46 +0900

    JS 시작

---
+++ vending-machine/index.js
@@ -0,0 +1,8 @@
+const myMoney = document.querySelector(".txt-mymoney");
+const listInput = document.querySelector(".list-input");
+function onInput() {
+  // const listInput = document.getElementsByClassName("list-input")[0].value;
+  //   const listInput = document.querySelector(".list-input").value;
+  listInput.value;
+  console.log(listInput);
+}
