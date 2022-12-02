38d32db33df0fac172e40858fa2426d9fe4cdbec
leeyeun <dpdms0109@naver.com>
2022-12-03 02:55:31 +0900

    파일 위치 수정

--- vending-machine/index.js
+++
@@ -1,8 +0,0 @@
-const myMoney = document.querySelector(".txt-mymoney");
-const listInput = document.querySelector(".list-input");
-function onInput() {
-  // const listInput = document.getElementsByClassName("list-input")[0].value;
-  //   const listInput = document.querySelector(".list-input").value;
-  listInput.value;
-  console.log(listInput);
-}
